3203133b5f230d52a2963222b81a28dbca07b660
trentrae <39933317+trentrae@users.noreply.github.com>
2023-03-28 09:09:29 -0500

    Create AzureRGDiscovery.ps1

---
+++ AzureRGDiscovery.ps1
@@ -0,0 +1,23 @@
+connect-azAccount
+$date = Get-Date -UFormat("%m-%d-%y")
+$currentDir = $(Get-Location).Path
+$oFile = "$($currentDir)\List_Of_All_Azure_Resources_$($date).csv"
+  
+if(Test-Path $oFile){
+    Remove-Item $oFile -Force
+}
+  
+"SUBSCRIPTION_NAME,SUBSCRIPTION_ID, RESOURCE_GROUP_NAME" | Out-File $oFile -Append -Encoding ascii
+  
+Get-AzSubscription | ForEach-Object{
+    $subscriptionId = $_.Id
+    $subscriptionName = $_.Name
+      
+    Set-AzContext -SubscriptionId $subscriptionId
+    Get-AzResourceGroup | ForEach-Object{
+        $resourceGroupName = $_.ResourceGroupName
+                     
+              
+            "$subscriptionName,$subscriptionId,$resourceGroupName" | Out-File $oFile -Append -Encoding ascii
+        }
+    }
